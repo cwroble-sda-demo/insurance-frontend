c4007e9dcdf406fe4c2fb25fea8ed22b21dcf259
cwroble <39490177+cwroble@users.noreply.github.com>
2023-08-18 16:16:21 -0500

    Update HomeHero.tsx

--- src/components/heroes/HomeHero.tsx
+++ src/components/heroes/HomeHero.tsx
@@ -20,7 +20,7 @@ const HomeHero = () => {
             <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
               <span className="block text-white">Get better care</span>
               <span className="block text-blue-200 font-semibold">
-                It is your life
+                It is your life, do it right
               </span>
             </h1>
             <p className="mt-6 max-w-lg mx-auto text-center text-xl text-blue-200 sm:max-w-3xl">
